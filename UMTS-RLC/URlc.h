3064f8fcabbd4f423130edff2cd7c28f294556ea
hamed <hamed.kermani1986@gmail.com>
2024-06-11 13:59:42 +0330

    remove comments

--- UMTS-RLC/URlc.h
+++ UMTS-RLC/URlc.h
@@ -10,20 +10,17 @@ class URlc{
 public:
     static void initRLCConfigs();
 
-    static void macPushUpRxCCCH(char* bit_pdu, int pdu_len); // frame + frame len
-    static void macPushUpRXDCCH(char* bit_pdu, int pdu_len, RbId rbid, UeIdType id_type,int UEid); //frame + length + rb id + (urnti/crnti)
+    static void macPushUpRxCCCH(char* bit_pdu, int pdu_len); 
+    static void macPushUpRXDCCH(char* bit_pdu, int pdu_len, RbId rbid, UeIdType id_type,int UEid); 
 
-    static RlcSdu* rrcRecvDCCH(); //sdu + len + ue id + rbid
-    static RlcSdu* rrcRecvCCCH();//sdu + len
+    static RlcSdu* rrcRecvDCCH(); 
+    static RlcSdu* rrcRecvCCCH();
 
-    static void rrcSendDCCH(char* sdu,int sdu_len, UeIdType id_type, int UEid, RbId rbid,std::string desc);//(id = urnti/crnti)
+    static void rrcSendDCCH(char* sdu,int sdu_len, UeIdType id_type, int UEid, RbId rbid,std::string desc);
     static void rrcSendCCCH(char* sdu,int sdu_len, std::string desc); //sdu  + len // all ccch except rrc conn setup
     static void rrcSendRRCConnectionSetup(uint32_t urnti, uint16_t crnti,char* sdu,int sdu_len); //only rrc conn set up
 
-    static ::std::vector<RlcPdu*> macReadTx(); // pdu + pdu len + rbid + ueid(crnti/urnti) + cccd/dcch
-
-
-
+    static ::std::vector<RlcPdu*> macReadTx(); 
 private:
     static ::std::queue<RlcSdu*> RxCCCHQueue;
 
